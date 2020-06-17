3eaff7d151c2bc9af5cf041864bfccfa05c6ad06
Aaron DeLory <rexiosdev@gmail.com>
2020-06-17 16:38:20 -0400

    Fixed Twitch4J logging

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 import com.github.philippheuer.credentialmanager.CredentialManagerBuilder
+import com.github.philippheuer.credentialmanager.domain.AuthenticationController
+import com.github.philippheuer.credentialmanager.domain.IdentityProvider
+import com.github.philippheuer.credentialmanager.identityprovider.OAuth2IdentityProvider
+import com.github.twitch4j.TwitchClient
 import com.github.twitch4j.TwitchClientBuilder
 import com.github.twitch4j.auth.providers.TwitchIdentityProvider
 import tornadofx.*
@@ -6,19 +10,22 @@ import tornadofx.*
 class TornadoApp : App(MainView::class)
 
 class MainView : View() {
+    private val controller = MainController()
     override val root = group { label("Hello World") }
 }
 
 class MainController : Controller() {
     private val credentialManager = CredentialManagerBuilder.builder().build()
-    private val twitchClient = TwitchClientBuilder.builder()
-        .withEnableHelix(true)
-        .withEnablePubSub(true)
-        .withCredentialManager(credentialManager)
-        .withEventManager(null)
-        .build()
+    private val twitchClient: TwitchClient? = null
+}
 
-    init {
-        credentialManager.registerIdentityProvider(TwitchIdentityProvider())
+class TestAuthenticationController : AuthenticationController() {
+    override fun startOAuth2AuthorizationCodeGrantType(
+        p0: OAuth2IdentityProvider?,
+        p1: String?,
+        p2: MutableList<Any>?
+    ) {
+        TODO("Not yet implemented")
     }
+
 }
