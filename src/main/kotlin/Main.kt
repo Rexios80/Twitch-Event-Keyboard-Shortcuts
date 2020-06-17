4a3c90232cef16838e33026802d42fdfc87d29e1
Aaron DeLory <rexiosdev@gmail.com>
2020-06-17 15:56:02 -0400

    Worked on Twitch4J setup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.github.philippheuer.credentialmanager.CredentialManagerBuilder
+import com.github.twitch4j.TwitchClientBuilder
+import com.github.twitch4j.auth.providers.TwitchIdentityProvider
+import tornadofx.*
+
+class TornadoApp : App(MainView::class)
+
+class MainView : View() {
+    override val root = group { label("Hello World") }
+}
+
+class MainController : Controller() {
+    private val credentialManager = CredentialManagerBuilder.builder().build()
+    private val twitchClient = TwitchClientBuilder.builder()
+        .withEnableHelix(true)
+        .withEnablePubSub(true)
+        .withCredentialManager(credentialManager)
+        .withEventManager(null)
+        .build()
+
+    init {
+        credentialManager.registerIdentityProvider(TwitchIdentityProvider())
+    }
 }
