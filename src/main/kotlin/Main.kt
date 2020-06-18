a10826333b694fcbd78b3563c54f7435d6c5c6e8
Aaron DeLory <rexiosdev@gmail.com>
2020-06-18 12:04:23 -0400

    Twitch4J works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
-import com.github.philippheuer.credentialmanager.CredentialManagerBuilder
-import com.github.philippheuer.credentialmanager.domain.AuthenticationController
-import com.github.philippheuer.credentialmanager.domain.IdentityProvider
-import com.github.philippheuer.credentialmanager.identityprovider.OAuth2IdentityProvider
-import com.github.twitch4j.TwitchClient
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential
+import com.github.philippheuer.events4j.simple.SimpleEventHandler
+import com.github.philippheuer.events4j.simple.domain.EventSubscriber
 import com.github.twitch4j.TwitchClientBuilder
-import com.github.twitch4j.auth.providers.TwitchIdentityProvider
+import com.github.twitch4j.pubsub.events.ChannelPointsRedemptionEvent
+import okhttp3.OkHttpClient
 import tornadofx.*
+
 
 class TornadoApp : App(MainView::class)
 
@@ -15,17 +15,31 @@ class MainView : View() {
 }
 
 class MainController : Controller() {
-    private val credentialManager = CredentialManagerBuilder.builder().build()
-    private val twitchClient: TwitchClient? = null
+    private val accessToken = AccessToken(Secret.clientId, Secret.secret, OkHttpClient())
+    private val credential: OAuth2Credential get() = OAuth2Credential(null, accessToken.accessToken)
+    private val twitchClient = TwitchClientBuilder.builder()
+        .withEnableHelix(true)
+        .withEnablePubSub(true)
+        .withDefaultAuthToken(credential)
+        .withClientId(Secret.clientId)
+        .withClientSecret(Secret.secret)
+        .build()
+
+    init {
+        twitchClient.clientHelper.enableFollowEventListener(Secret.channel)
+        twitchClient.eventManager.getEventHandler(SimpleEventHandler::class.java).registerListener(TeksEventHandler())
+
+        val channelName = Secret.channel
+        val resultList = twitchClient.helix.getUsers(accessToken.accessToken, null, listOf(channelName)).execute()
+        val channelId = resultList.users.find { it.displayName == channelName }?.id
+
+        twitchClient.pubSub.listenForChannelPointsRedemptionEvents(credential, channelId)
+    }
 }
 
-class TestAuthenticationController : AuthenticationController() {
-    override fun startOAuth2AuthorizationCodeGrantType(
-        p0: OAuth2IdentityProvider?,
-        p1: String?,
-        p2: MutableList<Any>?
-    ) {
-        TODO("Not yet implemented")
+class TeksEventHandler {
+    @EventSubscriber
+    fun handleChannelPointsRedemption(event: ChannelPointsRedemptionEvent) {
+        println("event: " + event.redemption.reward.title)
     }
-
 }
